27b4d3024d382dff325214ec9bbb93c6eb16502b
Alexandre R. Saccol <arsaccol@gmail.com>
2022-05-05 17:53:25 -0300

    We insert rough text standing in for 'twats' inside the twat feed

--- twatter.js
+++ twatter.js
@@ -15,22 +15,44 @@ async function get_users()
     return users
 }
 
-//function TwatComponent({userId, id, title})
-//{
-//    const twatElement = new HTMLDivElement()
-//
-//    usernam
-//}
+
+// (we show the username of the user with id equal to userId from post)
+function TwatComponent({userId, id, body}, users)
+{
+    const twatElement = document.createElement('div')
+    twatElement.innerText = body
+
+    return twatElement
+}
+
+function addTwatElements(twatElements)
+{
+    const twatFeedElement = document.getElementById('twat-feed')
+    console.log(`Twat Feed: ${twatFeedElement}`)
+
+
+    const twats = []
+    for(let i = 0; i < 100; ++i) 
+        twats.push(TwatComponent({userId: 123, id: 1234, body: 'Das ist twat!'}, []))
+
+    console.log(twats)
+    twats.forEach(twat => twatFeedElement.appendChild(twat))
+}
 
 
 
 
-(async () => {
+const main = async () => {
     const posts =  await get_posts()
-    //console.log(posts[0])
+    console.log(posts[0])
 
-    const users = await get_users()
-    console.log(users[0])
-})()
+    //const users = await get_users()
+    //console.log(users[0])
+
+    console.log('buga buga')
+    addTwatElements([])
+}
 
 console.log('buga buga')
+
+window.onload = main
